5d1f0dc13c184eee019c0fae050c241136d70633
demiro <damir21@hotmail.com>
2019-10-31 18:34:38 +0000

    scaffolded components and setup general layout styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/shared/app-header/app-header.component';
+import { AppFooterComponent } from './components/shared/app-footer/app-footer.component';
+import { UserFormComponent } from './components/shared/user-form/user-form.component';
+import { UserTableComponent } from './components/shared/user-table/user-table.component';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    UserFormComponent,
+    UserTableComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
